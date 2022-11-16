bc97a9da5cd73d5494712a0e17d8ea3904c1164d
Vinicius <vinicius.bergamasco@hotmail.com>
2022-11-16 17:16:33 -0300

    Commit inicial

---
+++ src/application/Program.java
@@ -0,0 +1,14 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Position position = new Position(1,1);
+		System.out.println(position.toString());
+	}
+
+}
